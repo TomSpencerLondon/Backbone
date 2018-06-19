e901816428da299151aece030dbb1881bdacb7d4
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-06-19 07:36:51 +0100

    Add get method for simple Vehicle model with Car inheritance and registration number validation

--- js/main.js
+++ js/main.js
@@ -3,19 +3,42 @@
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-var Animal = Backbone.Model.extend({
-  walk: function(){
-    console.log("Animal is walking...");
+// var Animal = Backbone.Model.extend({
+//   walk: function(){
+//     console.log("Animal is walking...");
+//   }
+// });
+
+// var Dog = Animal.extend({
+//   walk: function(){
+//     Animal.prototype.walk.apply(this);
+//     console.log("Dog is walking...")
+//   }
+// });
+
+// var dog = new Dog(); 
+
+// dog.walk();
+
+
+var Vehicle = Backbone.Model.extend({
+  
+  urlRoot: "/api/vehicles",
+  validate: function(attrs){
+    if(!attrs.registrationNumber)
+        return "Registration is required";
+  }, 
+  start: function(){
+    console.log("Vehicle started");
   }
 });
 
-var Dog = Animal.extend({
-  walk: function(){
-    Animal.prototype.walk.apply(this);
-    console.log("Dog is walking...")
+var Car = Vehicle.extend({
+  start: function(){
+    console.log(`Car with registration number ${this.get("registrationNumber")} started.`)
   }
-});
+})
 
-var dog = new Dog(); 
+car = new Car({registrationNumber: "XLI887", 
+              color: "Blue"}); 
 
-dog.walk();
